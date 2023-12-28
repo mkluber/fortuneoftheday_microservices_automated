2a7d158797c544c412a046fbd76fe5678731a347
Mark Kluber <145130822+mkluber@users.noreply.github.com>
2023-12-28 14:16:14 +0100

    .

---
+++ main.tf
@@ -0,0 +1,135 @@
+provider "aws" {
+  region = var.region
+}
+
+module "dynamodb-table" {
+  source  = "terraform-aws-modules/dynamodb-table/aws"
+
+
+  name                        = var.dynamodb_table_name
+  hash_key                    = var.dynamodb_table_hash_key
+  range_key                   = var.dynamodb_table_range_key
+  table_class                 = "STANDARD"
+  deletion_protection_enabled = false
+
+  attributes = [
+    {
+      name = var.dynamodb_table_hash_key
+      type = "S"
+    },
+    {
+      name = var.dynamodb_table_range_key
+      type = "S"
+    }
+  ]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "staging"
+  }
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.cidr
+
+  azs             = var.azs
+  public_subnets = var.public_subnets
+  create_igw = "true"
+  default_route_table_name = var.default_route_table_name
+  map_public_ip_on_launch = "true"
+}
+
+module "elastic-beanstalk-application" {
+  source  = "cloudposse/elastic-beanstalk-application/aws"
+  version = "0.11.1"
+
+}
+
+module "elastic-beanstalk-environment" {
+  source  = "cloudposse/elastic-beanstalk-environment/aws"
+  version = "0.51.2"
+
+  description                = var.description
+  region                     = var.region
+  availability_zone_selector = var.availability_zone_selector
+  dns_zone_id                = var.dns_zone_id
+  dns_subdomain              = var.dns_subdomain
+
+  wait_for_ready_timeout             = var.wait_for_ready_timeout
+  elastic_beanstalk_application_name = module.elastic_beanstalk_application.elastic_beanstalk_application_name
+  environment_type                   = var.environment_type
+  loadbalancer_type                  = var.loadbalancer_type
+  elb_scheme                         = var.elb_scheme
+  tier                               = var.tier
+  version_label                      = var.version_label
+  force_destroy                      = var.force_destroy
+
+  instance_type    = var.instance_type
+  root_volume_size = var.root_volume_size
+  root_volume_type = var.root_volume_type
+
+  autoscale_min             = var.autoscale_min
+  autoscale_max             = var.autoscale_max
+  autoscale_measure_name    = var.autoscale_measure_name
+  autoscale_statistic       = var.autoscale_statistic
+  autoscale_unit            = var.autoscale_unit
+  autoscale_lower_bound     = var.autoscale_lower_bound
+  autoscale_lower_increment = var.autoscale_lower_increment
+  autoscale_upper_bound     = var.autoscale_upper_bound
+  autoscale_upper_increment = var.autoscale_upper_increment
+
+  vpc_id               = module.vpc.vpc_id
+  loadbalancer_subnets = module.vpc.public_subnet_ids
+  application_subnets  = module.vpc.public_subnet_ids
+
+  allow_all_egress = true
+
+  additional_security_group_rules = [
+    {
+      type                     = "ingress"
+      from_port                = 0
+      to_port                  = 65535
+      protocol                 = "-1"
+      source_security_group_id = module.vpc.vpc_default_security_group_id
+      description              = "Allow all inbound traffic from trusted Security Groups"
+    }
+  ]
+
+  rolling_update_enabled  = var.rolling_update_enabled
+  rolling_update_type     = var.rolling_update_type
+  updating_min_in_service = var.updating_min_in_service
+  updating_max_batch      = var.updating_max_batch
+
+  healthcheck_url  = var.healthcheck_url
+  application_port = var.application_port
+
+  solution_stack_name = var.solution_stack_name    
+  additional_settings = var.additional_settings
+  env_vars            = var.env_vars
+
+  extended_ec2_policy_document = data.aws_iam_policy_document.fortune_dynamodb_permissions.json
+  prefer_legacy_ssm_policy     = false
+  prefer_legacy_service_policy = false
+  scheduled_actions            = var.scheduled_actions
+
+  context = module.this.context
+}
+
+data "aws_iam_policy_document" "fortune_dynamodb_permissions" {
+    statement {
+      sid = "fortune_dynamodb_permissions"
+      actions = [
+                "dynamodb:PutItem",
+                "dynamodb:DescribeTable",
+                "dynamodb:DeleteItem",
+                "dynamodb:GetItem",
+                "dynamodb:Scan",
+                "dynamodb:Query",
+                "dynamodb:UpdateItem"
+    ]
+    resources = ["${module.dynamodb-table.dynamodb_table_arn}"]
+  }
+}
---
+++ terraform.auto.tfvars
@@ -0,0 +1,103 @@
+region = "eu-central-1"
+
+cidr = "137.137.0.0/16"
+azs = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+public_subnets = ["137.137.1.0/24", "137.137.2.0/24", "137.137.3.0/24"]
+vpc_name = "fortunevpc"
+default_route_table_name = "fortuneroute"
+dynamodb_table_name = "Fortunes"
+dynamodb_table_hash_key = "FortuneName"
+dynamodb_table_range_key = "FortuneOrigin"
+
+availability_zones = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+
+# namespace = "eg"
+
+# stage = "test"
+
+# name = "eb-env"
+
+description = "Terraform Fortune App"
+
+tier = "WebServer"
+
+environment_type = "LoadBalanced"
+
+loadbalancer_type = "application"
+
+availability_zone_selector = "Any 3"
+
+instance_type = "t2.micro"
+
+autoscale_min = 1
+
+autoscale_max = 2
+
+wait_for_ready_timeout = "20m"
+
+force_destroy = true
+
+rolling_update_enabled = true
+
+rolling_update_type = "Health"
+
+updating_min_in_service = 0
+
+updating_max_batch = 1
+
+healthcheck_url = "/"
+
+application_port = 80
+
+root_volume_size = 8
+
+root_volume_type = "gp3"
+
+autoscale_measure_name = "CPUUtilization"
+
+autoscale_statistic = "Average"
+
+autoscale_unit = "Percent"
+
+autoscale_lower_bound = 20
+
+autoscale_lower_increment = -1
+
+autoscale_upper_bound = 80
+
+autoscale_upper_increment = 1
+
+elb_scheme = "public"
+
+// https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html
+// https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html#platforms-supported.docker
+solution_stack_name = "64bit Amazon Linux 2023 v4.0.7 running Python 3.11"
+
+version_label = "1"
+
+dns_zone_id = "Z01280511PKX9H52OLIQH"
+dns_subdomain = "api"
+
+// https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
+additional_settings = [
+  {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "StickinessEnabled"
+    value     = "false"
+  },
+  {
+    namespace = "aws:elasticbeanstalk:managedactions"
+    name      = "ManagedActionsEnabled"
+    value     = "false"
+  }
+]
+
+env_vars = {
+  "DB_HOST"         = "xxxxxxxxxxxxxx"
+  "DB_USERNAME"     = "yyyyyyyyyyyyy"
+  "DB_PASSWORD"     = "zzzzzzzzzzzzzzzzzzz"
+  "ANOTHER_ENV_VAR" = "123456789"
+}
+
+s3_bucket_versioning_enabled = false
+enable_loadbalancer_logs     = false
